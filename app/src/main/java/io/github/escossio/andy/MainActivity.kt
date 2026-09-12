939066d2550185cf90510087f2edbc4bd5b97a24
escossio <166327237+escossio@users.noreply.github.com>
2026-09-12 19:37:14 -0300

    feat: initialize device identity on launch

--- app/src/main/java/io/github/escossio/andy/MainActivity.kt
+++ app/src/main/java/io/github/escossio/andy/MainActivity.kt
@@ -1,6 +1,7 @@
 package io.github.escossio.andy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -9,15 +10,30 @@ import androidx.compose.foundation.text.BasicText
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import io.github.escossio.andy.core.deviceidentity.DeviceIdentityResult
+import io.github.escossio.andy.data.deviceidentity.AndroidDeviceIdentityFactory
 
 internal const val BOOTSTRAP_TEXT = "Andy"
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        initializeDeviceIdentity()
         setContent {
             AndyBootstrap()
         }
+    }
+
+    private fun initializeDeviceIdentity() {
+        when (val result = AndroidDeviceIdentityFactory.create(applicationContext).ensureIdentity()) {
+            is DeviceIdentityResult.Ready -> Log.i(LOG_TAG, "DEVICE_IDENTITY_READY")
+            is DeviceIdentityResult.Unavailable ->
+                Log.w(LOG_TAG, "DEVICE_IDENTITY_UNAVAILABLE:${result.reason.name}")
+        }
+    }
+
+    private companion object {
+        const val LOG_TAG = "AndyDeviceIdentity"
     }
 }
 
