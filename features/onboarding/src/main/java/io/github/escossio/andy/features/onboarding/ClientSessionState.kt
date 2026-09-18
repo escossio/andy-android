17f457d94d19d4f6c2b45fa091ce7ea40628aa70
Leonardo Escossio <leonardo.escossio@gmail.com>
2026-09-18 15:33:04 -0300

    feat: add V0.3C client session state

---
+++ features/onboarding/src/main/java/io/github/escossio/andy/features/onboarding/ClientSessionState.kt
@@ -0,0 +1,28 @@
+package io.github.escossio.andy.features.onboarding
+
+import io.github.escossio.andy.sdk.clientapi.AuthenticatedClientBootstrap
+
+sealed interface ClientSessionState {
+    data object Idle : ClientSessionState
+    data object Establishing : ClientSessionState
+    data object LoadingBootstrap : ClientSessionState
+    data class Connected(val bootstrap: AuthenticatedClientBootstrap) : ClientSessionState
+    data class Failure(val reason: ClientSessionFailure) : ClientSessionState
+}
+
+enum class ClientSessionFailure {
+    DEVICE_IDENTITY_UNAVAILABLE,
+    NETWORK_FAILURE,
+    CLIENT_SESSION_DEVICE_REJECTED,
+    CLIENT_SESSION_CHALLENGE_NOT_FOUND,
+    CLIENT_SESSION_CHALLENGE_EXPIRED,
+    CLIENT_SESSION_CHALLENGE_CONSUMED,
+    CLIENT_SESSION_SIGNATURE_INVALID,
+    CLIENT_SESSION_ACTIVE_TENANT_REQUIRED,
+    CLIENT_SESSION_TENANT_FORBIDDEN,
+    CLIENT_SESSION_UNAUTHENTICATED,
+    CLIENT_SESSION_AUTHORITY_REJECTED,
+    CLIENT_SESSION_UNAVAILABLE,
+    SESSION_BOOTSTRAP_MISMATCH,
+    UNEXPECTED_RESPONSE,
+}
